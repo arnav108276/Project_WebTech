3f94c8b6a5ec60dbb2a9d94e4322ed404584ebba
Arnav Goel <143831459+arnav108276@users.noreply.github.com>
2024-03-14 15:24:40 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,40 +1,29 @@
- // Initialize an array to store user data
         var users = [];
-
-        // Function to handle signup
         function signup() {
             var email = document.getElementById("email").value;
             var password = document.getElementById("password").value;
-
-            // Create an object representing the user
             var user = {
                 email: email,
                 password: password
             };
-
-            // Add the user object to the users array
             users.push(user);
-
-            // Optionally, you can save the users array to local storage for persistence
             window.location.href = "index.html";
             alert("User signed up successfully!");
             window.location.href = "index.html#shop-items";
         }
-
-        // Function to handle login
         function login() {
             var email = document.getElementById("email").value;
             var password = document.getElementById("password").value;
-
-            // Loop through the users array to find a matching user
-            for (var i = 0; i < users.length; i++) {
+         var len=users.length;
+            for (var i = 0; i < len; i++) {
                 if (users[i].email === email && users[i].password === password) {
                     // Redirect to index.html with anchor to specific element
                     window.location.href = "home.html";
                     return;
                 }
+                else{
+                  alert("Invalid email or password. Please try again.");
+                }
             }
-
-            // If no matching user is found
-            alert("Invalid email or password. Please try again.");
+           
         }
