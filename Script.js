69f8b657059172b0ae3d8a69c22c0bbe5e62f5d4
Arnav Goel <143831459+arnav108276@users.noreply.github.com>
2024-03-19 20:29:36 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -16,14 +16,19 @@ var users = [];
             var email = document.getElementById("email").value;
             var password = document.getElementById("password").value;
             var len=users.length;
+            var isLoggedIn = false;
             for (var i = 0; i < len; i++) {
-                if (users[i].email != email && users[i].password != password) {
-                     window.alert("Invalid email or password. Please try again.");
-                    // return;
-                }
-                else{
-                   window.location.href = "index.html #shop-items";
+                if (users[i].email === email && users[i].password === password) {
+                    isLoggedIn = true;
+                    break;
                 }
             }
+            
+            if (isLoggedIn) {
+                window.location.href = "index.html #shop-items";
+            } else {
+                window.alert("Invalid email or password. Please try again.");
+            }
+            
            
         }
